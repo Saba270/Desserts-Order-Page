418ca324ee0604220dfde153782bce7c694df195
Saba270 <saba27012007Gmail.com>
2024-12-08 21:51:56 +0400

    Added images for dessert listings

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@ import { useState } from 'react'
 import './App.css'
 import { AddToCart, CarbonNeutral, DecrementQuantity, IncrementQuantity, OrderConfirmed, RemoveItem, EmptyCart } from './components/Icons'
 import Dessert from './components/Dessert'
-import dessertsData from './data.json'
+import desserts from './data.json'
+import { use } from 'react'
 
 function App() {
+  const [dessertsData, setDessertsData] = useState(desserts.map((dessert) => {
+    return { ...dessert, quantity: 0 }
+  }))
 
-  const baseURL = 'https://res.cloudinary.com/dc2c49xov/desserts/'
 
   return (
     <>
       <section className='desserts-list-section'>
         <h1>Desserts</h1>
-        <div>{dessertsData.map((dessert) => {
-          return <Dessert name={dessert.name}/>
+        <div>{dessertsData.map((currentDessert) => {
+          console.log(currentDessert)
+          return <Dessert dessert={currentDessert} changeData={setDessertsData} Data={dessertsData}/>
         })}</div>
       </section>
       <section>
--- src/components/Dessert.jsx
+++ src/components/Dessert.jsx
@@ -1,7 +1,17 @@
-const Dessert = ({ name }) => {
-    return (
+import { AddToCart, DecrementQuantity, IncrementQuantity } from "./Icons"
+
+const baseURL = 'https://res.cloudinary.com/dc2c49xov/desserts/'
+
+const Dessert = ({ dessert, changeData, Data}) => {
+    return ( 
         <div className="dessert">
-            <h1>{name}</h1>
+            {dessert.quantity === 0 ? <button onClick={() => {}}><AddToCart/> Add to Cart</button> : <button><DecrementQuantity/> {dessert.quantity} <IncrementQuantity/></button>}
+            <picture>
+                <source media="(max-width: 375px)" srcset={baseURL + dessert.images.mobile} />
+                <source media="(max-width: 768px)" srcset={baseURL + dessert.images.tablet} />
+                <img src={baseURL + dessert.images.desktop} alt="" />
+            </picture>
+
         </div>
     )
 }
