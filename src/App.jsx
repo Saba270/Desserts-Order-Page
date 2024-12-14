a34a5c10d7737596bf7c85b983ebaed3615086d0
Saba270 <saba27012007Gmail.com>
2024-12-14 11:54:30 +0400

    Added style and dessert info to the listings

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
         })}</div>
       </section>
       <section>
-
+        
       </section>
     </>
   )
--- src/components/Dessert.jsx
+++ src/components/Dessert.jsx
@@ -1,4 +1,4 @@
-import { AddToCart, DecrementQuantity, IncrementQuantity,RemoveItem } from "./Icons"
+import { AddToCart, DecrementQuantity, IncrementQuantity, RemoveItem } from "./Icons"
 
 const baseURL = 'https://res.cloudinary.com/dc2c49xov/desserts/'
 
@@ -8,10 +8,14 @@ const Dessert = ({ dessert, changeQuantity }) => {
             <picture>
                 <source media="(max-width: 375px) and (orientation: portrait)" srcset={baseURL + dessert.images.mobile} />
                 <source media="(max-width: 768px) and (orientation: portrait)" srcset={baseURL + dessert.images.tablet} />
-                <img src={baseURL + dessert.images.desktop} alt="dessert-image" />
+                <img className={`${dessert.quantity > 0 ? "selected" : ""}`} src={baseURL + dessert.images.desktop} alt="dessert-image" />
                 {dessert.quantity === 0 ? <button className="dessert-btn add-to-cart-btn" onClick={() => { changeQuantity(dessert, 1) }}><AddToCart /> Add to Cart</button> : <figure className="dessert-btn change-amount-btn"><button onClick={() => { changeQuantity(dessert, -1) }}><DecrementQuantity /></button> {dessert.quantity} <button onClick={() => { changeQuantity(dessert, 1) }}><IncrementQuantity /></button></figure>}
             </picture>
-
+            <div className="dessert-info">
+                <p className="dessert-category">{dessert.category}</p>
+                <h4 className="dessert-name">{dessert.name}</h4>
+                <h4 className="dessert-price">${dessert.price.toFixed(2)}</h4>
+            </div>
         </div>
     )
 }
